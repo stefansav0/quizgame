d9635e8a08bdb386a0e1b2b82f7bab2edab30665
stefansav0 <ravikumar.kumar2333@gmail.com>
2026-05-13 12:47:19 +0530

    fixed view on blog page

--- app/api/blogs/[slug]/route.js
+++ app/api/blogs/[slug]/route.js
@@ -32,7 +32,11 @@ export async function GET(request, { params }) {
     // Await params before destructuring (Required for Next.js 15+)
     const { slug } = await params;
 
-    const blog = await Blog.findOne({ slug });
+    const blog = await Blog.findOneAndUpdate(
+  { slug },
+  { $inc: { views: 1 } },
+  { new: true }
+);
 
     if (!blog) {
       return NextResponse.json(
--- models/Blog.js
+++ models/Blog.js
@@ -48,6 +48,10 @@ const blogSchema = new mongoose.Schema(
       type: String, 
       required: true 
     },
+    views: {
+      type: Number,
+      default: 0,
+    },
   },
   { timestamps: true }
 );
